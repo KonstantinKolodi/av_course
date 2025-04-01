8d95e1742071e0a977bef7663b73ecc8717605a5
KonstantinKolodi <kokolo@taltech.ee>
2025-04-01 15:53:18 +0300

    Adding README file and my_robot_controller for final evaluation

---
+++ my_robot_controller/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'my_robot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # World files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+    ],
+    install_requires=['setuptools', 'rclpy'],
+    zip_safe=True,
+    maintainer='konstantin',
+    maintainer_email='kokolo@taltech.ee',
+    description='A robot controller package for ROS 2',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "mapping = my_robot_controller.mapping:main",
+        ],
+    },
+)
+
