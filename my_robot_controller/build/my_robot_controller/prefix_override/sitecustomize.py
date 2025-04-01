a46182e70383642f86add4d85f337fa532a52ee8
KonstantinKolodi <kokolo@taltech.ee>
2025-04-01 15:46:55 +0300

    Deleting everything

--- my_robot_controller/build/my_robot_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/konstantin/ros2_ws/src/my_robot_controller/install/my_robot_controller'
